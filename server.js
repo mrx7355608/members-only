49898d9e6f35ed5bdc70ebf2773a1cf3f2b7d4ca
mrx7355608 <mrx7355608@gmail.com>
2022-10-28 05:28:13 +0500

    hosting issue test

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const server = createServer(app);
 
 const startServer = function () {
     // Connecting to database
+    console.log(process.env);
     mongoose.connect(process.env.DATABASE_URL);
     console.log("[INFO] Connected to database");
     mongoose.connection.on("error", function () {
